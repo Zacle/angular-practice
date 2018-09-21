e2f87e9e54dc78779409a7be7e56535e4316a082
zacle <zacharienziuki@gmail.com>
2018-09-21 02:15:03 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '330966014132750',
+        clientSecret: '0c4507577bbc389a909c21ceaefe298b'
+    }
 }
