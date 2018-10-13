737a2051a528553539dc5879ba5fc553281801fd
zacle <zacharienziuki@gmail.com>
2018-10-13 13:23:11 +0200

    Integrating Client and Server

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var favoriteRouter = require('./routes/favorites');
 const uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
@@ -58,6 +59,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites', favoriteRouter),
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
